95a2016000ad2efb82a529aed6a08270c7478536
Linda Chang <lindachang186@gmail.com>
2019-03-19 22:20:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  get 'hello_world', to 'static#hello_world'
 
   def hello_world
     render
